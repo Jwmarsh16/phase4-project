53e5427708366a07b98251270092e1b8298e1a5e
Jonathan Marshall <jwmarsh16@gmail.com>
2024-07-31 13:54:55 -0500

    created models and relationships

---
+++ server/migrations/versions/577732c8418b_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 577732c8418b
+Revises: 
+Create Date: 2024-07-30 18:14:59.712264
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '577732c8418b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
